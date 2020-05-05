b0727afc8f8931f6ba35781db175d3c86ada1018
Eugene Xu <eugene_xu@epam.com>
2020-05-05 15:40:34 +0800

    fix search url not exists

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
-const search = document.querySelector('.searchbar');
-const date = document.querySelector('.date');
-const time = document.querySelector('.time');
-const settingsIcon = document.querySelector('.settings-icon');
-const settingsPanel = document.querySelector('.settings');
-const preSearch = '> ';
+const search = document.querySelector(".searchbar");
+const date = document.querySelector(".date");
+const time = document.querySelector(".time");
+const settingsIcon = document.querySelector(".settings-icon");
+const settingsPanel = document.querySelector(".settings");
+const preSearch = "> ";
 const links = {};
 
-const db = () => JSON.parse(localStorage.getItem('links'));
+const db = () => JSON.parse(localStorage.getItem("links"));
 const getLink = (key) => db()[key];
 
 const setLink = (key, value) => {
   links[key] = value;
-  localStorage.setItem('links', JSON.stringify(links));
+  localStorage.setItem("links", JSON.stringify(links));
   updateQuickLinkDom();
-}
+};
 
-const typeHandler = () => search.value = search.value.includes(preSearch) ? search.value : `${preSearch}`;
+const typeHandler = () =>
+  (search.value = search.value.includes(preSearch)
+    ? search.value
+    : `${preSearch}`);
 
 const searchHandler = () => {
   let rawSearchValue = search.value.split(preSearch)[1];
-  const searchUrl = getLink(rawSearchValue.split(' ')[0])
-  if (true) {
-    rawSearchValue = rawSearchValue.replace(rawSearchValue.split(' ')[0], '').trim();
-    window.location.replace(`${searchUrl + rawSearchValue.replace(/ /g, '+')}`);
+  const searchUrl = getLink(rawSearchValue.split(" ")[0]);
+  if (searchUrl !== undefined) {
+    console.log(rawSearchValue.split(" ")[0]);
+    rawSearchValue = rawSearchValue
+      .replace(rawSearchValue.split(" ")[0], "")
+      .trim();
+    console.log(searchUrl);
+    console.log(rawSearchValue);
+    window.location.replace(`${searchUrl + rawSearchValue.replace(/ /g, "+")}`);
+  } else {
+    window.location.reload();
   }
 };
 
 const searchInit = () => {
   search.value = preSearch;
   search.focus();
-  search.addEventListener('input', typeHandler);
-  search.addEventListener('change', searchHandler);
+  search.addEventListener("input", typeHandler);
+  search.addEventListener("change", searchHandler);
 };
 
 function checkTime(i) {
@@ -42,61 +52,62 @@ function checkTime(i) {
 
 function startTime() {
   var today = new Date();
-  date.innerText = today.toJSON().slice(0,10);
+  date.innerText = today.toJSON().slice(0, 10);
   var h = today.getHours();
   var m = today.getMinutes();
   var s = today.getSeconds();
   m = checkTime(m);
   s = checkTime(s);
   time.innerHTML = h + ":" + m + ":" + s;
-  t = setTimeout(function() {
-    startTime()
+  t = setTimeout(function () {
+    startTime();
   }, 500);
 }
 
 const settingsInit = () => {
-  settingsIcon.addEventListener('click', () => {
-    settingsPanel.classList.toggle('visible');
-    settingsIcon.classList.toggle('visible');
+  settingsIcon.addEventListener("click", () => {
+    settingsPanel.classList.toggle("visible");
+    settingsIcon.classList.toggle("visible");
     searchInit();
-  })
-}
+  });
+};
 
-const storageInit = (forced=true) => {
-  const links = JSON.parse(localStorage.getItem('links'));
+const storageInit = (forced = true) => {
+  const links = JSON.parse(localStorage.getItem("links"));
   if (!links || forced) {
     localStorage.clear();
-    localStorage.setItem('links', {});
-    setLink('go', 'https://www.google.com/search?q=');
-    setLink('gh', 'https://github.com/search?q=');
-    setLink('yt', 'https://www.youtube.com/results?search_query=');
-    setLink('mdn', 'https://developer.mozilla.org/en/search?q=');
-    setLink('bd', 'https://www.baidu.com/s?wd=');
+    localStorage.setItem("links", {});
+    setLink("go", "https://www.google.com/search?q=");
+    setLink("gh", "https://github.com/search?q=");
+    setLink("yt", "https://www.youtube.com/results?search_query=");
+    setLink("mdn", "https://developer.mozilla.org/en/search?q=");
+    setLink("bd", "https://www.baidu.com/s?wd=");
+    setLink("bing", "https://cn.bing.com/search?q=");
   }
-}
+};
 
 const updateQuickLinkDom = () => {
-  while(settingsPanel.firstChild){
-      settingsPanel.removeChild(settingsPanel.firstChild)
+  while (settingsPanel.firstChild) {
+    settingsPanel.removeChild(settingsPanel.firstChild);
   }
-  const title = document.createElement('p');
-  title.innerText = 'quick links';
-  title.classList = 'tagname';
+  const title = document.createElement("p");
+  title.innerText = "quick links";
+  title.classList = "tagname";
   settingsPanel.appendChild(title);
-  Object.entries(db()).forEach(link => {
-    const tempLink = document.createElement('div');
-    tempLink.classList = 'quicklink';
-    const key = document.createElement('input');
-    key.classList = 'key';
+  Object.entries(db()).forEach((link) => {
+    const tempLink = document.createElement("div");
+    tempLink.classList = "quicklink";
+    const key = document.createElement("input");
+    key.classList = "key";
     key.value = link[0];
-    const value = document.createElement('input');
-    value.classList = 'value'
+    const value = document.createElement("input");
+    value.classList = "value";
     value.value = link[1];
     tempLink.appendChild(key);
     tempLink.appendChild(value);
     settingsPanel.appendChild(tempLink);
-  })
-}
+  });
+};
 
 (() => {
   searchInit();
@@ -104,4 +115,4 @@ const updateQuickLinkDom = () => {
   settingsInit();
   storageInit();
   updateQuickLinkDom();
-})()
+})();
